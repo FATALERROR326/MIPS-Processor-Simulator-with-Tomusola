7596387fb08088f34042afb839f05cd25124ca70
FATALERROR326 <59378806+FATALERROR326@users.noreply.github.com>
2021-11-27 15:28:40 -0500

    Update main.cpp

--- ECE668 Project/Project 4/main.cpp
+++ ECE668 Project/Project 4/main.cpp
@@ -3,203 +3,73 @@
 #include <numeric>
 #include <stdio.h>
 #include <string.h>
+#include "RS.hpp"
+#include "InstTable.hpp"
+#include "Reg.hpp"
+
 using namespace std;
 
-/**
-    Structure for storing information in each RS row
- */
-struct Station
-{
-    int busy;
-    //for INTfu, FPadder, FPmult
-    int optype;
-    int Vj;
-    int Vk;
-    int Qj;
-    int Qk;
-    //for load
-    int address;
-};
+static const int REGISTER_NUM = 10;
+static const int MEMORY_LENGTH = 500;
 
-struct InstStats
-{
-    int type;
-    int source1;
-    int source2;
-    int des;
-    int issueStage;
-    int exeStage;
-    int wbStage;
-    int instNum;
-};
- 
-class RS
-{
-public:
-    int *NumSta;//[#number of integer FU, FP adder, FP mult, Load, store]
-    struct Station *fu;
+// for both RS type and EXE type
+const int INTEGER = 1;
+const int INTEGER_LOOP = 2;
+const int FU_ADDER_ADD = 3;
+const int FU_ADDER_MINUS = 4;
+const int FU_MULT_MULT = 5;
+const int FU_MULT_DIVID = 6;
+const int LOAD = 7;
+const int SAVE = 8;
+
+bool hasDependency(RegResStat* regResStat, InstTable* instTable, int instNum) {
     
-    //struct *Station st;//unaware of storestation now
-    RS(int *nums);
-    int* getRSnum(void);
-    struct Station* getRS(void);
-    int checkRS(int type);
-    int insertRS(int type);
-
-};
-/**
-    Used to store instructions and their execution stages
- */
-class InstTable
-{
-public:
-    int currInstNum = 0;
-    int finishNum = 0;
-    int stackSize = 0;
-    InstStats* insts;
-    InstTable(InstStats* insts);
-    InstTable();
-    void setIssue(int cycle, int instNum);
-    void setExe(int cycle, int instNum);
-    void setWb(int cycle, int instNum);
-};
-
-/**
-    Used to store avalibility of registers
- */
-class Registers
-{
-    int* regFlags;
-public:
-    Registers(int num);
-    int getFlag(int regNum);
-};
-
-
-RS:: RS(int *nums)
-{
-    NumSta = nums;
-    int all = accumulate(NumSta, NumSta+5, 0);
-    struct Station f[all];
-    fu = f;
-    for (int i = 0; i < all; i ++)
-    {
-        fu[i].busy = 0;
+    int source1 = instTable->insts[instNum].source1;
+    int source2 = instTable->insts[instNum].source2;
+    bool flag1 = false, flag2 = false;
+    //positive is immediate number, negative is register number
+    if(source1 < 0) {
+        flag1 = regResStat->getFlag(-source1) == 1;
+    } else {
+        flag1 = false;
     }
-
-    /*
-    for (int i = 0; i < all; i++)
-    {
-        Station a{busy:0};
-        fu[i] = a;
+    if(source2 < 0) {
+        flag2 = regResStat->getFlag(-source2) == 1;
+    } else {
+        flag2 = false;
     }
-    */
-    //st
+    return flag1 || flag2;
 }
 
-int* RS:: getRSnum(void)
-{
-    
-    return NumSta;
+void loadInstructions(char** insts) {
+    //TODO: load instructions into instruction array
 }
 
-struct Station* RS:: getRS(void)
-{
-    
-    return fu;
+void decode(InstTable* instTable, string* instructions) {
+    //TODO: transfer binary code in instructions to information in instTable->insts
 }
 
-int RS:: checkRS(int type)
-{
-    int i = 0;
-    int start = 0;
-    for (int j = 0; j < type; j++)
-    {
-        start += NumSta[j];
-    }
-    //cout <<'.' <<start << endl;
-    //cout <<','<< NumSta[type]<<endl;
-    int end = start + NumSta[type];
-    if (!fu[start].busy) return start;
-    for (i = start; i < end; i++)
-    {
-        //cout << fu[i].busy << endl;
-        //cout << i << ','<< endl;
-        if (!fu[i].busy)
-        {
-            //cout << ','<< endl;
-            break;
-        }
-    }
-    if (i == start) return -1;
-    return i;
-}
-
-int RS:: insertRS(int type)
-{
-    //should also input decoded instruction for fill operands and others.
-    int index = checkRS(type);
-    if (index != -1)
-    {
-        //cout << fu[index].busy << '.' << index << endl;
-        fu[index].busy = 1;
-        //cout << fu[index].busy << ',' << index << endl;
-    }
-    return index;
-}
-
-InstTable:: InstTable(InstStats* insts)
-{
-    this->insts = insts;
-}
-
-void InstTable:: setIssue(int cycle, int instNum)
-{
-    this->insts[instNum].issueStage = cycle;
-}
-
-void InstTable:: setExe(int cycle, int instNum)
-{
-    this->insts[instNum].exeStage = cycle;
-}
-
-void InstTable:: setWb(int cycle, int instNum)
-{
-    this->insts[instNum].wbStage = cycle;
-}
-
-Registers:: Registers(int num)
-{
-    this->regFlags = (int*)malloc(num * sizeof(int));
-}
-int Registers:: getFlag(int regNum)
-{
-    return this->regFlags[regNum];
-}
-
-bool hasDependency(Registers* registers, InstTable* instTable) {
-    
-    registers.getFlag();
-}
-
-void display(RS* rs) {
-    
-}
 int main()
 {
+    //
     //initializations
     int capacity[5] = {2,2,2,2,2};
+    // cycles consumed by each type of execution
+    int typeCycle[10] = {1,1,2,3,4,5,6,1,2,2};
     RS *rs = new RS(capacity);
     InstTable *instTable = new InstTable();
-    Registers *registers = new Registers();
+    RegResStat *regResStat = new RegResStat(REGISTER_NUM);
+    
     int finishNum = instTable->finishNum;
     int stackSize = instTable->stackSize;
     int cycle = 0;
     int endNum = finishNum + 1;
+    int* registers = (int*)malloc(sizeof(int) * REGISTER_NUM);
+    int* memory = (int*)malloc(sizeof(int) * MEMORY_LENGTH);
     bool haveIssuedThisCycle = true;
     //TODO: manage the input
     
-    while(finishNum <= stackSize) {
+    while(finishNum < stackSize) {
         /* for each cycle */
         cycle ++;
         haveIssuedThisCycle = false;
@@ -207,18 +77,28 @@ int main()
         for(int instNum = finishNum; instNum < endNum; ++ instNum) {
             /* to operate each line in instruction table
              */
+            InstStats inst = instTable->insts[instNum];
             // when rs is not full, issue the new NEXT instruction
-            if(!rs->isFull() && instTable->isIssued() && !haveIssuedThisCycle) {
-                instTable->setIssue(instNum, cycle + 1);
+            if(!(rs->checkRS(1) == -1) && !haveIssuedThisCycle) {
+                instTable->setIssue(cycle + 1, instNum);
+                rs->insertRS(1);
                 haveIssuedThisCycle = true;
             } else {
                 continue;
             }
             //if there is no dependencies, do the execution
-            if(instTable->isIssued() && cycle >= instTable->getIssueCycle() && hasDependency()) {
+            if(instTable->isIssued(cycle, instNum) && !instTable->isExecuted(cycle, instNum) && !hasDependency(regResStat, instTable, instNum)) {
+                instTable->setExe(cycle + typeCycle[inst.type], instNum);
+            }
+            // if execution has been done, do writeback and update register
+            if(instTable->isExecuted(cycle, instNum) && !instTable->isWriten(cycle, instNum)){
+                instTable->setWb(cycle + 1, instNum);
+                if(inst.type == 0) {
+                    registers[inst.des] = registers[inst.source1] + registers[inst.source2];
+                    regResStat->regFlags[inst.des] = -1;
+                }
                 
             }
-            
         }
         if(++ endNum > stackSize) {
             -- endNum;
@@ -226,6 +106,4 @@ int main()
             -- endNum;
         }
     }
-    int index1 = rs->insertRS(1);
-    cout << index1 << endl;
 }
